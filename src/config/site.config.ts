187567e1271b06ccb1842c2dff4d77854e228071
Orif-92 <sarniyozovorif@gmail.com>
2023-04-17 23:10:12 +0300

    Async thunk redux

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -2,11 +2,11 @@ export const siteConfig = {
 	metaData: {
 		title: 'Orif | Programming Courses',
 		description:
-			'Free programming lessons and projects, exercises to strengthen algorithms. Ready-made templates, personal profile portfolio and resume. Learning JavaScript related technologies. ORIF',
+			'Free programming lessons and projects, exercises to strengthen algorithms. Ready-made templates, personal profile portfolio and resume. Learning JavaScript related technologies. SAMMI',
 		author: 'Orif Sarniyozov',
 		keyword:
 			"orif sarniyozov, orif, dasturlash kurslari, dasturlashga oid darslar, reactjs uzbek tilida, vuejs uzbek tilida, redux uzbek tilida, sammi, sammi academy, bepul dasturlash, rezyume yozish, portfolio, sammi javascript, sammi raqamli avlod, javascript, reactjs, vuejs, javascript darslari, reactjs darslari, vuejs darslari, dasturlash darslari, o'zbek tilida dasturlash, reactjs o'zbek tilida, reactjs darslari o'zbek tilida, javascript darslari, javascript darslari o'zbek tilida, dasturash darslari o'zbek tilida, dasturlashni o'rganish, dasturlash, IT loyihalar o'zbek tilida",
-		ogImage: 'https://cdn.dribbble.com/users/33271/screenshots/3975282/media/e803cde680942ba882d6e8db8cb79071.gif',
+		ogImage: 'https://media.graphassets.com/BW4BRUnCQJWCazTdKnVQ',
 	},
 	baseURL: 'https://sammi.ac/',
 	logo: '/images/logo.svg',
