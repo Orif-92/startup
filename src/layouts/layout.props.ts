22c9576f563187f4b78a0e7cc70af6369112f2bd
Orif-92 <sarniyozovorif@gmail.com>
2023-05-17 21:05:55 +0300

    Books category

--- src/layouts/layout.props.ts
+++ src/layouts/layout.props.ts
@@ -1,5 +1,15 @@
 import { ReactNode } from 'react';
+import { BooksType } from 'src/interfaces/books.interface';
+import { CourseType } from 'src/interfaces/course.interface';
+import { InstructorType } from 'src/interfaces/instructor.interface';
 
 export interface LayoutProps {
 	children: ReactNode;
+}
+
+export interface AppProviderProps {
+	courses: CourseType[];
+	course: CourseType;
+	instructors: InstructorType[];
+	books: BooksType[];
 }
